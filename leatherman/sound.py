e71eff3fe05b8586cbaf7454f3b3407da992d253
Chris Norman <chris.norman2@googlemail.com>
2017-09-09 07:53:45 +0100

    Start playing new_stream immediately.

--- leatherman/sound.py
+++ leatherman/sound.py
@@ -9,7 +9,8 @@ new_stream = None
 
 def play(stream):
     """Play a stream."""
-    global old_stream, new_stream
-    old_stream = new_stream
+    global new_stream
+    if new_stream is not None:
+        new_stream.pause()
     new_stream = stream
     new_stream.play()
