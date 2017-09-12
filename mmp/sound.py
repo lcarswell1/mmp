db36fcb3f3d766b1a11d21bae9631fdd724577d4
Chris Norman <chris.norman2@googlemail.com>
2017-09-12 22:31:30 +0100

    Update position in realtime.

--- mmp/sound.py
+++ mmp/sound.py
@@ -1,11 +1,17 @@
 """Sound-related stuff."""
 
 import logging
+from time import time
+import wx
 from sound_lib.output import Output
 from attr import attrs, attrib
 from .jobs import add_job
+from . import app
 
 logger = logging.getLogger(__name__)
+last_run = 0
+run_every = 0.1
+zeroed = False
 
 
 @attrs
@@ -23,9 +29,39 @@ old_stream = None
 new_stream = None
 
 
+def update_ui():
+    """Update user interface components."""
+    global zeroed
+    if not new_stream:
+        state = None
+        if not zeroed:
+            zeroed = True
+            app.frame.left_panel.position.SetValue(0)
+    else:
+        stream = new_stream.stream
+        if stream.is_playing:
+            state = 'Playing'
+        elif stream.is_paused:
+            state = 'Paused'
+        elif stream.is_stopped:
+            state = 'Stopped'
+        app.frame.left_panel.position.SetValue(
+            100 / stream.get_length() * stream.position
+        )
+    title = app.name
+    if state is not None:
+        title = '%s [%s]' % (title, state)
+    app.frame.SetTitle(title)
+
+
 def play_manager():
     """A job to check the status of playing streams and play the next one if
     the old one has finished."""
+    global last_run
+    now = time()
+    if now - last_run >= run_every:
+        last_run = now
+        wx.CallAfter(update_ui)
     if new_stream is not None:
         if new_stream.stream.position >= new_stream.stream.get_length():
             # new_stream has finished. Let's get a new track from the queue.
